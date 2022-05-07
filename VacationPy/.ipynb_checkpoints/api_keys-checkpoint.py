a115b5376fafc124c46c7c4f4654b495e5858ce3
Vanessa Gonzales <101536552+Vgonzales1382@users.noreply.github.com>
2022-05-07 15:15:52 -0500

    final homework

---
+++ VacationPy/.ipynb_checkpoints/api_keys-checkpoint.py
@@ -0,0 +1,5 @@
+# OpenWeatherMap API Key
+weather_api_key = "b2a519cc64e4fca7346d62a91d496c01"
+
+# Google API Key
+g_key = "AIzaSyB-BrF_Rdh69VE5ukD9HwOIccIdbwyKJK8"
---
+++ WeatherPy/.ipynb_checkpoints/api_keys-checkpoint.py
@@ -0,0 +1,5 @@
+# OpenWeatherMap API Key
+weather_api_key = "b2a519cc64e4fca7346d62a91d496c01"
+
+# Google API Key
+g_key = "YOUR KEY HERE!"
